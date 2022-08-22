6ce9f41d6537a3d4410085f6bb5b2f233c3dda74
Steinar <s.r.eriksen@gmail.com>
2022-08-22 14:43:01 +0200

    Adding setup

--- energydeskclient/assets/assetowners_api.py
+++ energydeskclient/assets/assetowners_api.py
@@ -11,8 +11,22 @@ class AssetOwnersApi:
     def save_ownerships(api_connection, ownership_graph_json):
        json_res=api_connection.exec_post_url('/api/asset-ownership/save-ownership-graph/', ownership_graph_json)
 
-
     @staticmethod
     def load_ownerships(api_connection):
         json_res = api_connection.exec_get_url('/api/asset-ownership/load-ownership-graph/')
+        return json_res
+
+    @staticmethod
+    def get_asset_ownerships(api_connection):
+        json_res = api_connection.exec_get_url('/api/asset-ownership/get-asset-ownerships/')
+        return json_res
+
+    @staticmethod
+    def get_asset_ownerships_pivoted(api_connection):
+        json_res = api_connection.exec_get_url('/api/asset-ownership/get-asset-ownerships-pivoted/')
+        return json_res
+
+    @staticmethod
+    def all_asset_ownership_paths(api_connection):
+        json_res = api_connection.exec_get_url('/api/asset-ownership/all-ownership-paths/')
         return json_res
