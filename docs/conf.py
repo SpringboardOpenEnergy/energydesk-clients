70b020120a262a87979bd81038057fd5527c9b0e
steinar <s.r.eriksen@gmail.com>
2022-08-24 09:43:56 +0200

    updated path

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -57,4 +59,4 @@ html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['./_build/html/_static']
