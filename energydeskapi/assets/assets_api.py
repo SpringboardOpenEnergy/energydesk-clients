71bf585631067b668171967419444913ff9cc6fe
Steinar E <s.r.eiksen@gmail.com>
2022-09-10 21:12:16 +0200

    Added location query

--- energydeskapi/assets/assets_api.py
+++ energydeskapi/assets/assets_api.py
@@ -4,10 +4,43 @@ import logging
 import pandas as pd
 logger = logging.getLogger(__name__)
 
+#fields = ['pk', 'asset_id', 'extern_asset_id', 'description', 'asset_type', 'grid_connection', 'power_supplier',
+ #         'asset_owner', 'asset_manager', 'meter_id', 'sub_meter_id', 'vendor', 'is_active']
+
+
+class Asset:
+    def __init__(self):
+        self.pk=0
+        self.asset_id=None
+        self.extern_asset_id=None
+        self.description=""
+        self.asset_type=None
+        self.grid_company=None
+        self.power_supplier=None
+        self.asset_owner=None
+        self.asset_manager=None
+        self.meter_id=""
+        self.sub_meter_id=""
+        self.vendor=None
+        self.is_active=True
+
+
+
 class AssetsApi:
     """ Class for assets
 
     """
+
+    @staticmethod
+    def get_asset_type_url(api_connection, asset_type_enum):
+        return api_connection.get_base_url() + '/api/assets/assettype/' + str(asset_type_enum.value) + "/"
+
+    @staticmethod
+    def get_asset_type(api_connection, asset_type_enum):
+        json_res = api_connection.exec_get_url('/api/assets/assettype/' + str(asset_type_enum.value)) + "/"
+        if json_res is None:
+            return None
+        return json_res
 
     # This function returns a single price (avg) for the period requested
     @staticmethod
@@ -21,7 +54,12 @@ class AssetsApi:
         """
         logger.info("Registering " + str(len(asset_list) )+ " assets")
         for asset in asset_list:
-            json_res=api_connection.exec_post_url('/api/assets/asset/', asset)
+            payload={}
+            if asset.asset_id is not None: payload['asset_id']=asset.asset_id
+            if asset.extern_asset_id is not None: payload['extern_asset_id'] = asset.extern_asset_id
+            if asset.description is not None: payload['description'] = asset.description
+            if asset.asset_manager is not None: payload['asset_manager'] = asset.asset_manager
+            json_res=api_connection.exec_post_url('/api/assets/asset/', payload)
 
 
     @staticmethod
--- energydeskapi/examples/asset_samples.py
+++ energydeskapi/examples/asset_samples.py
@@ -1,7 +1,7 @@
 import logging
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.assets.assets_api import AssetsApi
-
+from energydeskapi.types.asset_enum_types import AssetTypeEnum
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("energydesk_client.log"),
@@ -13,8 +13,10 @@ logging.basicConfig(level=logging.INFO,
 def query_asset_info(api_conn):
     df=AssetsApi.get_asset_types(api_conn)
     print("Asset types", df)
-    df=AssetsApi.get_assets_ext(api_conn)
-    print("Asset list", df)
+    #df=AssetsApi.get_assets_ext(api_conn)
+    #print("Asset list", df)
+    u=AssetsApi.get_asset_type_url(api_conn, AssetTypeEnum.HYDRO)
+    print(u)
 
 
 if __name__ == '__main__':
